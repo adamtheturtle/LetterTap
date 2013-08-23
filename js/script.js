ff4e97fe1d74b709b3ebfa0435a4a1fe0d6472ee
Adam Dangoor <adamdangoor@gmail.com>
2013-08-23 02:03:58 +0100

    Include JS at the end of the HTML file. Get closer to support for changeable alphabets

--- js/script.js
+++ js/script.js
@@ -1,32 +1,40 @@
 jQuery(document).ready(function($) {
     (function() {
-        var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L',
-                        'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-        position = 0;
+        var alphabet_normal_order = ['A','B','C','D','E','F','G','H',
+                                     'I','J','K','L', 'M','N','O','P',
+                                     'Q','R','S','T','U','V','W','X',
+                                     'Y','Z'];
+        var position = 0;
+        var num_timer_loops = 0;
+        var current_alphabet = alphabet_normal_order;
+        var milliseconds_per_change = 200;
         $letter = $('#letter');
         $current_word = $('#current_word'),
         self = this;
 
         this.nextLetter = function() {
-            position = (position + 1) % alphabet.length;
-            $letter.text(alphabet[position]);
+            position = (position + 1) % current_alphabet.length;
+            $letter.text(current_alphabet[position]);
         };
 
         this.resetLetter = function() {
             position = 0;
-            $letter.text(alphabet[position]);
+            num_timer_loops = 0;
+            $letter.text(current_alphabet[position]);
         };
 
         this.timer = function() {
-            var timer_milliseconds = 200;
             setTimeout(function() {
-                self.nextLetter();
+                if( (num_timer_loops + 1) % milliseconds_per_change == 0) {
+                    self.nextLetter();
+                }
+                num_timer_loops++;
                 self.timer();
-            }, timer_milliseconds)
+            }, 1)
         };
 
         this.addLetter = function() {
-            $current_word.text($current_word.text() + $letter.text())
+            $current_word.text($current_word.text() + $letter.text());
         };
 
         this.timer();
