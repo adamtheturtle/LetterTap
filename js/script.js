53fc0ef2e4e2f6f16c775128d1f5830d865ac96e
Adam Dangoor <adamdangoor@gmail.com>
2013-08-23 01:34:02 +0100

    No longer uses stylesheet (temporary). Current word works. Spaces instead of tabs indent

--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
 jQuery(document).ready(function($) {
-	(function() {
-		var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L',
-                        'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-			position = 0,
-			$letter = $('#letter'),
-            $current_word = $('#current_word'),
-			self = this;
+    (function() {
+        var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L',
+                        'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+        position = 0;
+        $letter = $('#letter');
+        $current_word = $('#current_word'),
+        self = this;
 
-		this.nextLetter = function() {
-			position++;
-			if (position > alphabet.length) {
-				position = 0;
-			}
+        this.nextLetter = function() {
+            position++;
+            if (position > alphabet.length) {
+                position = 0;
+            }
 
-			$letter.text(alphabet[position]);
-		};
-
-		this.resetLetter = function() {
-			position = 0;
-			$letter.text(alphabet[position]);
-		};
-
-		this.timer = function() {
-			setTimeout(function() {
-				self.nextLetter();
-				self.timer();
-			}, 2000)
-		};
-        this.addLetter = function() {
-            $current_word.text($current_word.text() + $letter.text())
-            
+            $letter.text(alphabet[position]);
         };
 
-		this.timer();
+        this.resetLetter = function() {
+            position = 0;
+            $letter.text(alphabet[position]);
+        };
 
-		$letter.click(function() {
+        this.timer = function() {
+            setTimeout(function() {
+                self.nextLetter();
+                self.timer();
+            }, 2000)
+        };
+
+        this.addLetter = function() {
+            $current_word.text($current_word.text() + $letter.text())
+        };
+
+        this.timer();
+
+        $letter.click(function() {
             self.addLetter();
-			self.resetLetter();
-			return false;
-		});
-
-	})();
+            self.resetLetter();
+            return false;
+        });
+    })();
 
 });
