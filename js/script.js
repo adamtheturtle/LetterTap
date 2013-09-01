3f0272f55cd67e869fd08dcd48153f76ea9ef4ce
Adam Dangoor <adamdangoor@gmail.com>
2013-09-01 10:11:09 +0100

    Uses var for all current variables, so they are in the correct scope

--- js/script.js
+++ js/script.js
@@ -13,24 +13,24 @@ $(document).ready(function() {
     var current_alphabet = alphabet_normal_order;
     var slowest_speed_seconds_per_change = 8;
     var milliseconds_per_change;
-    $letter = $('#letter');
-    $letter_container = $('#letter_container');
-    $current_word = $('#current_word');
-    $clear_button = $('#clear_button');
-    $speed_slider = $('#speed_slider');
-    $alphabet_normal_radio = $('#alphabet_normal_radio');
-    $alphabet_frequency_radio = $('#alphabet_frequency_radio');
-    $alphabet_form = $('#alphabet_form');
+    var letter = $('#letter');
+    var letter_container = $('#letter_container');
+    var current_word = $('#current_word');
+    var clear_button = $('#clear_button');
+    var speed_slider = $('#speed_slider');
+    var alphabet_normal_radio = $('#alphabet_normal_radio');
+    var alphabet_frequency_radio = $('#alphabet_frequency_radio');
+    var alphabet_form = $('#alphabet_form');
 
     nextLetter = function() {
         position = (position + 1) % current_alphabet.length;
-        $letter.text(current_alphabet[position]);
+        letter.text(current_alphabet[position]);
     };
 
     resetLetter = function() {
         position = 0;
         num_timer_loops = 1;
-        $letter.text(current_alphabet[position]);
+        letter.text(current_alphabet[position]);
     };
 
     timer = function() {
@@ -44,23 +44,23 @@ $(document).ready(function() {
     };
 
     addLetter = function() {
-        $current_word.text($current_word.text() + $letter.text());
+        current_word.text(current_word.text() + letter.text());
     };
 
     setSpeed = function() {
         slowest_speed = slowest_speed_seconds_per_change * 100;
-        milliseconds_per_change = Math.round(slowest_speed / $speed_slider.val());
+        milliseconds_per_change = Math.round(slowest_speed / speed_slider.val());
     };
 
     clearWord = function() {
-        $current_word.text('');
+        current_word.text('');
         resetLetter();
     };
 
     changeAlphabet = function() {
-        if($alphabet_normal_radio.is(':checked')) {
+        if(alphabet_normal_radio.is(':checked')) {
             current_alphabet = alphabet_normal_order;
-        } else if($alphabet_frequency_radio.is(':checked')) {
+        } else if(alphabet_frequency_radio.is(':checked')) {
             current_alphabet = alphabet_by_frequency;
         }
 
@@ -71,20 +71,20 @@ $(document).ready(function() {
     setSpeed();
     timer();
 
-    $letter_container.click(function() {
+    letter_container.click(function() {
         addLetter();
         resetLetter();
     });
     
-    $speed_slider.change(function() {
+    speed_slider.change(function() {
         setSpeed();
     });
     
-    $alphabet_form.change(function() {
+    alphabet_form.change(function() {
         changeAlphabet();
     });
 
-    $clear_button.click(function() {
+    clear_button.click(function() {
         clearWord();
     });
 });
