3e094585a1fa4046a139a81955536256c905b709
Adam Dangoor <adamdangoor@gmail.com>
2013-09-03 22:38:20 +0100

    Reworked to use jQuery Mobile UI Framework

--- js/script.js
+++ js/script.js
@@ -1,90 +1,95 @@
-$(document).ready(function() {
-    var alphabet_normal_order = ['A','B','C','D','E','F','G','H',
-                                 'I','J','K','L', 'M','N','O','P',
-                                 'Q','R','S','T','U','V','W','X',
-                                 'Y','Z'];
+$('#writing_page').bind('pageinit', function() {
+    var alphabet_normal_order = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                                 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                                 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    var alphabet_by_frequency = ['E','T','A','O','I','N','S','H',
-                                 'R','D','L','C','U','M','W','F',
-                                 'G','Y','P','B','V','K','J','X',
-                                 'Q','Z'];
+    var alphabet_by_frequency = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R',
+                                 'D', 'L', 'C', 'U', 'M', 'W', 'F', 'G', 'Y',
+                                 'P', 'B', 'V', 'K', 'J', 'X', 'Q', 'Z'];
     var position = 0;
-    var num_timer_loops = 1;
     var current_alphabet = alphabet_normal_order;
     var slowest_speed_seconds_per_change = 8;
-    var milliseconds_per_change;
     var letter = $('#letter');
     var letter_container = $('#letter_container');
     var current_word = $('#current_word');
     var clear_button = $('#clear_button');
     var speed_slider = $('#speed_slider');
-    var alphabet_normal_radio = $('#alphabet_normal_radio');
-    var alphabet_frequency_radio = $('#alphabet_frequency_radio');
     var alphabet_form = $('#alphabet_form');
-
-    nextLetter = function() {
-        position = (position + 1) % current_alphabet.length;
-        letter.text(current_alphabet[position]);
-    };
+    var settings_page = $('#settings_page');
+    var time_between_changes = 2600;
 
     resetLetter = function() {
         position = 0;
-        num_timer_loops = 1;
         letter.text(current_alphabet[position]);
     };
 
     timer = function() {
         setTimeout(function() {
-            if( num_timer_loops % milliseconds_per_change == 0) {
-                nextLetter();
-            }
-            num_timer_loops++;
+            position = (position + 1) % current_alphabet.length;
+            letter.text(current_alphabet[position]);
             timer();
-        }, 10)
+        }, time_between_changes)
     };
 
     addLetter = function() {
         current_word.text(current_word.text() + letter.text());
-    };
-
-    setSpeed = function() {
-        slowest_speed = slowest_speed_seconds_per_change * 100;
-        milliseconds_per_change = Math.round(slowest_speed / speed_slider.val());
+        clear_button.show();
     };
 
     clearWord = function() {
         current_word.text('');
         resetLetter();
+        clear_button.hide();
     };
 
-    changeAlphabet = function() {
-        if(alphabet_normal_radio.is(':checked')) {
+    changeAlphabet = function(selected_alphabet) {
+        if (selected_alphabet === 'normal') {
             current_alphabet = alphabet_normal_order;
-        } else if(alphabet_frequency_radio.is(':checked')) {
+        } else if (selected_alphabet === 'frequency') {
             current_alphabet = alphabet_by_frequency;
         }
 
         resetLetter();
     };
 
-    changeAlphabet();
-    setSpeed();
     timer();
+    clearWord();
+    $(document).bind('touchmove', false);
+
+    settings_page.bind('pageinit', function() {
+        var speed_slider = $('#speed_slider');
+        speed_slider.change(function() {
+            speed_scale = $(this).val();
+            time_between_changes = slowest_speed_seconds_per_change * 1000 / speed_scale;
+        });
+    });
+
+    settings_page.on("pageshow", function() {
+        var normal_alphabet_radio = $('#normal-alphabet')
+        var frequency_alphabet_radio = $('#frequency-alphabet')
+
+        if (current_alphabet === alphabet_normal_order) {
+            normal_alphabet_radio.attr("checked", true).checkboxradio("refresh");
+            frequency_alphabet_radio.attr("checked", false).checkboxradio("refresh");
+        } else if (current_alphabet === alphabet_by_frequency) {
+            normal_alphabet_radio.attr("checked", false).checkboxradio("refresh");
+            frequency_alphabet_radio.attr("checked", true).checkboxradio("refresh");
+        }
+
+    });
 
     letter_container.click(function() {
         addLetter();
         resetLetter();
     });
-    
-    speed_slider.change(function() {
-        setSpeed();
-    });
-    
+
     alphabet_form.change(function() {
-        changeAlphabet();
+        var selected_alphabet = $("#alphabet_form :radio:checked").val();
+        changeAlphabet(selected_alphabet);
     });
 
     clear_button.click(function() {
         clearWord();
     });
+
 });
